8c6882c4fbc88338e830332b6054dd51e6ada101
Peter Lamberjack <pashamanprofirus@gmail.com>
2023-12-10 21:53:14 +0300

    math update

--- pinelib/math.py
+++ pinelib/math.py
@@ -1,11 +1,66 @@
 import pinelib.series as pls
 import numpy as np
 import math
+
 math_abs = abs
 
-e = math.e
-pi = math.pi
-phi = math.phi
+e = 2.7182818284590452
+pi = 3.1415926535897932
+phi = 1.6180339887498948
+rphi = 0.6180339887498948
+
+
+def abs(value):
+    if isinstance(value, pls.Serie):
+        return pls.Serie(np.abs(value.data))
+    elif isinstance(value, (list, tuple)):
+        array = np.array(value)
+        return np.abs(array)
+    elif isinstance(value, (int, float)):
+        return math_abs(value)
+
+
+def acos(value):
+    if isinstance(value, pls.Serie):
+        return pls.Serie(np.arccos(value.data))
+    elif isinstance(value, (list, tuple)):
+        array = np.array(value)
+        return np.arccos(array)
+    elif isinstance(value, (int, float)):
+        return math.acos(value)
+
+
+def asin(value):
+    if isinstance(value, pls.Serie):
+        return pls.Serie(np.arcsin(value.data))
+    elif isinstance(value, (list, tuple)):
+        array = np.array(value)
+        return np.arcsin(array)
+    elif isinstance(value, (int, float)):
+        return math.asin(value)
+
+
+def atan(value):
+    if isinstance(value, pls.Serie):
+        return pls.Serie(np.arctan(value.data))
+    elif isinstance(value, (list, tuple)):
+        array = np.array(value)
+        return np.arctan(array)
+    elif isinstance(value, (int, float)):
+        return math.atan(value)
+
+
+def avg(*value):
+    if isinstance(value, pls.Serie):
+        return np.average(value.data)
+    elif isinstance(value, (list, tuple)):
+        return sum(value) / len(value)
+
+
+def ceil(value):
+    if isinstance(value, pls.Serie):
+        return np.ceil(value)
+
 
 def sqrt(value):
     if isinstance(value, pls.Serie):
@@ -54,43 +109,3 @@ def cos(value):
     elif isinstance(value, (list, tuple)):
         array = np.array(value)
         return np.cos(array)
-
-
-def abs(value):
-    if isinstance(value, pls.Serie):
-        return pls.Serie(np.abs(value.data))
-    elif isinstance(value, (list, tuple)):
-        array = np.array(value)
-        return np.abs(array)
-    elif isinstance(value, (int, float)):
-        return math_abs(value)
-
-
-def acos(value):
-    if isinstance(value, pls.Serie):
-        return pls.Serie(np.arccos(value.data))
-    elif isinstance(value, (list, tuple)):
-        array = np.array(value)
-        return np.arccos(array)
-    elif isinstance(value, (int, float)):
-        return math.acos(value)
-
-
-def asin(value):
-    if isinstance(value, pls.Serie):
-        return pls.Serie(np.arcsin(value.data))
-    elif isinstance(value, (list, tuple)):
-        array = np.array(value)
-        return np.arcsin(array)
-    elif isinstance(value, (int, float)):
-        return math.asin(value)
-
-
-def atan(value):
-    if isinstance(value, pls.Serie):
-        return pls.Serie(np.arctan(value.data))
-    elif isinstance(value, (list, tuple)):
-        array = np.array(value)
-        return np.arctan(array)
-    elif isinstance(value, (int, float)):
-        return math.atan(value)
--- pinelib/utils.py
+++ pinelib/utils.py
@@ -9,8 +9,10 @@ def fix_inf(array: np.ndarray) -> np.ndarray:
 
 def fit_arrays(array1: np.ndarray, array2: np.ndarray) -> tuple[np.ndarray]:
     if len(array1) > len(array2):
-        array2 = np.pad(array2, (0, len(array1)-len(array2)))
+        array1 = array1[:len(array2)]
+        #array2 = np.pad(array2, (0, len(array1)-len(array2)))
     elif len(array1) < len(array2):
-        array1 = np.pad(array1, (0, len(array2)-len(array1)))
+        array2 = array2[:len(array1)]
+        #array1 = np.pad(array1, (0, len(array2)-len(array1)))
     
     return array1, array2
